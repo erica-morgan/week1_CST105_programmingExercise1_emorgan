2230fd70c7f7f8d994e544c212118773e7ef1e93
erica-morgan <36687849+erica-morgan@users.noreply.github.com>
2018-02-25 22:53:53 -0700

    No changes made; finalizing commit to push.

---
+++ PrgExercise1.2/AboutMe.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package exerciseOne;
+
+/**
+ * @Program: exerciseOne Program
+ * @File: AboutMe.java
+ * @Summary: Introductory program
+ * @author: Erica Morgan
+ * @date: 02/25/2018
+ */
+public class AboutMe {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("My name is Erica.");
+		System.out.println("My favorite color is teal.");
+		System.out.println("If I won the lottery I would travel"
+				+ " with my family and donate my time to charity.");
+	}
+
+}
---
+++ PrgExercise1.3/Introduction.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package myFirstProject;
+
+/**
+ * @Program: myFirstProject program
+ * @File: Introduction.java
+ * @Summary: Java project as instructed
+ * @Author: Erica Morgan
+ * @Date: 02/25/2018
+ **/
+public class Introduction {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		// This is the class main method
+
+	}
+
+}
